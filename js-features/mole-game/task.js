7c2629f1ba1bca6fd2cacc1596ec6d8903205f8f
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-02-11 00:33:34 +0700

    done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,15 +1,15 @@
 (() => {
     getHole = i => document.getElementById(`hole${i}`);
     //const inputs = document.querySelectorAll('div > .hole');
-    for (let i = 0; i++; i < 10) {
+    for (let i = 1; i < 10; i++) {
         getHole(i).addEventListener("click", myFunction);
     }
 
     function myFunction() {
         const dead = document.getElementById("dead");
         const lost = document.getElementById("lost");
-        if (dead.textContent < 10 & lost.textContent < 5) {
-            if (hole.className.includes('hole_has-mole')
+        if (dead.textContent <= 8 & lost.textContent <= 3) {
+            if (this.className.includes('hole_has-mole')
                 // или this.className == "hole hole_has-mole"
             ) {
                 dead.textContent++;
@@ -17,7 +17,7 @@
                 lost.textContent++;
             }
         } else
-        if (dead.textContent >= 10) {
+        if (dead.textContent >= 8) {
             alert('Вы выиграли');
             dead.textContent = 0;
             lost.textContent = 0;
