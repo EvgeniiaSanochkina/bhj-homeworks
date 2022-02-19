0fb2fa07115883080faa6e41a6ede45331ac50c6
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-02-20 00:13:09 +0700

    done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,22 @@
 let menuItem = Array.from(document.querySelectorAll('.menu__item'));
 //let menuSub = Array.from(document.querySelectorAll('.menu_sub'));
 
+
 for (let i = 0; i < menuItem.length; i++) {
     if (menuItem[i].querySelector('.menu_sub')) {
-        menuItem[i].onclick = function() {
+        menuItem[i].querySelector('.menu__link').onclick = function() {
             menuItem[i].querySelector('.menu_sub').classList.toggle('menu_active');
             return false
         }
     }
 }
+
+
+//for (let i = 0; i < menuItem.length; i++) {
+//  if (menuItem[i].querySelector('.menu__link')) {
+//    menuItem[i].onclick = function() {
+//      menuItem[i].querySelector('.menu__link').classList.toggle('menu_active');
+//    return false
+// }
+// }
+//}
