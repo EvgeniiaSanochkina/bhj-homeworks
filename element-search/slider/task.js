22d08c1a2c284a749af9ef18283b4441300323aa
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-02-20 00:15:00 +0700

    done

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,36 +1,15 @@
 let sliderItem = Array.from(document.getElementsByClassName('slider__item'));
 let prev = document.querySelector('.slider__arrow_prev');
 let next = document.querySelector('.slider__arrow_next');
-let currentSlide = 0;
 
 next.onclick = function() {
-    if (currentSlide == (sliderItem.length - 1)) {
-        sliderItem[0].classList.add('slider__item_active');
-        sliderItem[sliderItem.length - 1].classList.remove('slider__item_active');
-        currentSlide = 0;
-    } else if (currentSlide == 0) {
-        sliderItem[1].classList.add('slider__item_active');
-        sliderItem[currentSlide].classList.remove('slider__item_active');
-        currentSlide++;
-    } else {
-        sliderItem[currentSlide + 1].classList.add('slider__item_active');
-        sliderItem[currentSlide].classList.remove('slider__item_active');
-        currentSlide++;
-    }
+    let index = sliderItem.findIndex((slider) => slider.classList.contains("slider__item_active"));
+    sliderItem[index].classList.remove("slider__item_active");
+    (index == (sliderItem.length - 1)) ? sliderItem[0].classList.add('slider__item_active'): sliderItem[index + 1].classList.add('slider__item_active');
 }
 
 prev.onclick = function() {
-    if (currentSlide == (sliderItem.length - 1)) {
-        sliderItem[currentSlide - 1].classList.add('slider__item_active');
-        sliderItem[currentSlide].classList.remove('slider__item_active');
-        currentSlide = sliderItem.length - 2;
-    } else if (currentSlide == 0) {
-        sliderItem[sliderItem.length - 1].classList.add('slider__item_active');
-        sliderItem[currentSlide].classList.remove('slider__item_active');
-        currentSlide = sliderItem.length - 1;
-    } else {
-        sliderItem[currentSlide - 1].classList.add('slider__item_active');
-        sliderItem[currentSlide].classList.remove('slider__item_active');
-        currentSlide--;
-    }
+    let index = sliderItem.findIndex((slider) => slider.classList.contains("slider__item_active"));
+    sliderItem[index].classList.remove("slider__item_active");
+    (index == 0) ? sliderItem[sliderItem.length - 1].classList.add('slider__item_active'): sliderItem[index - 1].classList.add('slider__item_active');
 }
