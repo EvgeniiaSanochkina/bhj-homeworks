5581017f6f6b9fffffd6db1ea6e562460e953612
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-03-08 14:54:12 +0700

    done

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -5,7 +5,8 @@ const list = document.querySelector('.tasks__list');
 add.addEventListener('click', addTask);
 
 function addTask() {
-    if (input.value) {
+    event.preventDefault();
+    if (input.value != " ") {
         list.insertAdjacentHTML(`beforeEnd`,
             `<div class = "task">
         <div class = "task__title"> ${input.value} </div> 
@@ -15,12 +16,17 @@ function addTask() {
         input.value = ``;
     }
 
-    const cross = Array.from(document.querySelectorAll(`.task__remove`));
-    const task = Array.from(document.querySelectorAll(`.task`));
+    //const cross = Array.from(document.querySelectorAll(`.task__remove`));
+    //const task = Array.from(document.querySelectorAll(`.task`));
 
-    for (let i = 0; i < cross.length; i++) {
-        cross[i].addEventListener('click', () => {
-            task[i].remove();
-        })
-    }
+    // for (let i = 0; i < cross.length; i++) {
+    //   cross[i].addEventListener('click', () => {
+    //     task[i].remove();
+    //})
+    //}
+
+    const task = Array.from(document.querySelectorAll(".task__remove"));
+    task[task.length - 1].addEventListener("click", () => {
+        list.removeChild(task[task.length - 1].closest(".task"));
+    })
 }
