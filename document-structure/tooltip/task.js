265f810754fab0ed6824b5f46d0dcbaad880c4aa
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-03-08 14:55:22 +0700

    done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,25 @@
-const tooltip = Array.from(document.querySelectorAll('.has-tooltip'));
-const tooltipDiv = document.createElement('div');
+const tooltips = Array.from(document.querySelectorAll(".has-tooltip"));
+//const tooltipDiv = document.createElement('div');
 
-tooltip.forEach((el) => {
-    el.addEventListener('click', function() {
-
-        tooltipDiv.classList.add('tooltip');
+tooltips.forEach((el) => {
+    const tooltipDiv = document.createElement('div');
+    tooltipDiv.innerText = `${el.title}`;
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+        tooltipDiv.classList.add("tooltip");
         tooltipDiv.setAttribute(`style`, `left: ${el.getBoundingClientRect().left}px; top: ${el.getBoundingClientRect().top + 16}px`);
-        tooltipDiv.classList.toggle('tooltip_active');
-        tooltipDiv.innerText = `${el.title}`;
-        el.insertAdjacentElement("afterend", tooltipDiv);
-
-        event.preventDefault();
+        //tooltipDiv.innerText = `${el.title}`;
+        el.insertAdjacentElement('beforeBegin', tooltipDiv);
+        const tooltipActive = Array.from(document.querySelectorAll(".tooltip_active"));
+        const findElem = tooltipActive.find(tooltipDiv => tooltipDiv.classList.contains("tooltip_active"));
+        console.log(findElem);
+        if (findElem === undefined) {
+            tooltipDiv.classList.add("tooltip_active");
+        } else {
+            if (findElem !== tooltipDiv) {
+                tooltipDiv.classList.add("tooltip_active");
+            }
+            findElem.classList.remove("tooltip_active");
+        }
     })
 })
