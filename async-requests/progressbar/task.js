88ff4c0a2f23ba15f69e804f1e11f376fe673c72
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-03-29 01:15:33 +0700

    done

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,15 +6,13 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     let formData = new FormData(form);
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    //xhr.send(formData);
+    xhr.upload.onprogress = function(e) {
+        progress.value = e.loaded / e.total;
+        //alert(`Отправлено ${e.loaded} из ${e.total} байт`);
+    };
     xhr.send(formData);
-
-});
-
-xhr.upload.onprogress = function(e) {
-    progress.value = 1;
-
-};
-
-//xhr.upload.addEventListener("progress", function (e) {
-//  progress.value = e.loaded / e.total;
-//})
+    xhr.upload.onload = function() {
+        alert(`Данные успешно отправлены.`);
+    };
+})
